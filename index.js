a1d6a57cc2e1adffc5479348d9c1dbb33623e925
Joey <joe>
2021-05-01 18:33:51 +0200

    update

--- index.js
+++ index.js
@@ -10,15 +10,17 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next();});
 
-app.get('/:twitchname' ,(req, res) => {
+app.get('/stream/:twitchname' ,(req, res) => {
     twitch.getStream(req.params.twitchname)
         .then(function(data){
             // var parsed = JSON.parse(data);
             // console.log(parsed)
-
+            console.log(data);
 
 
             res.send(`
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 
             <div class=".container-fluid">
@@ -56,6 +58,59 @@ app.get('/:twitchname' ,(req, res) => {
 
 
 });
+
+
+app.get('/stream/direct/:twitchname' ,(req, res) => {
+    url = `https://www.twitch.tv/${req.params.twitchname}`
+
+    twitch.getStream(url)
+        .then(function(data){
+            // var parsed = JSON.parse(data);
+            // console.log(parsed)
+            console.log(data);
+
+
+            res.send(`
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
+            <div class=".container-fluid">
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" value="${data[0].url}" id="myInput" width:1px; >
+
+                </div>
+                <div class="input-group mb-3">
+                <button class="btn btn-outline-secondary" id="button-addon1" onclick="myFunction()">Copy text and past above</button>
+
+                </div>
+            </div>   
+            <script> 
+            function myFunction() {
+
+            var copyText = document.getElementById("myInput");
+            copyText.select();
+            copyText.setSelectionRange(0, 99999);
+            document.execCommand("copy");
+            }
+            </script>
+
+            
+            
+            `)
+
+        })
+        .catch(err => res.send(err));
+
+    // .then(data => res.send(data))
+    // twitchStreams.get('channel')
+    // .then(function(streams) {
+    //     res.send(data)
+    // });
+
+
+});
+
 
 
 
