b90eb7fc95b5941db5a9b6d4fb56882854c89d67
dxvx <gdfg>
2021-05-07 22:50:22 +0200

    update

--- index.js
+++ index.js
@@ -111,7 +111,67 @@ app.get('/stream/direct/:twitchname' ,(req, res) => {
 
 });
 
+app.get('/login' ,(req, res) => {
 
+   
+
+
+            res.send(`
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
+           
+            <a id="login"
+            href="https://id.twitch.tv/oauth2/authorize?client_id=di5u96cpttmxapibsjpvmyr43x6hn3&redirect_uri=http%3A%2F%2Flocalhost%2Ftwitch_api_test%2Fhello%2Fwww%2F&response_type=token&scope=user:read:follows%20user:edit:follows%20user:read:subscriptions">Login</a>
+            
+            
+            `)
+
+      
+
+    // .then(data => res.send(data))
+    // twitchStreams.get('channel')
+    // .then(function(streams) {
+    //     res.send(data)
+    // });
+
+
+});
+app.get('/login/parm' ,(req, res) => {
+
+   
+
+
+    res.send(`
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <p id="acces_token"></p>
+
+  <script>
+
+function insert(){
+    var parsedHash = new URLSearchParams(window.location.hash.substr(1));
+    var access_token = parsedHash.get('access_token');
+    $("#acces_token").text(access_token);
+    document.getElementById('acces_token').innerHTML = "Some text to enter";
+
+}
+  </script>
+    
+    `)
+
+
+
+// .then(data => res.send(data))
+// twitchStreams.get('channel')
+// .then(function(streams) {
+//     res.send(data)
+// });
+
+
+});
 
 
 const PORT = process.env.PORT || 5000;
